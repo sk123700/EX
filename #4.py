1de553031feb533e41c9dcb98e0a7ec82dc9f35e
sk123700 <ping6269@naver.com>
2024-04-24 13:18:52 +0900

    Hello

---
+++ #4.py
@@ -0,0 +1,15 @@
+animals = []
+animals.append('cat')
+animals.append('dog')
+animals.append('mouse')
+#count=collections.Counter(animals)
+#rint(animals)
+#print(count)
+#count(animals)
+#count = [animals]
+#print(animals)len(animals)
+count = 3
+print(animals)
+#print(count)
+#len(animals)
+print(count)
