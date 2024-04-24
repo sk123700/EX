7e5b6c0be099bb907dc02d419bac99821beca5fa
sk123700 <165137055+sk123700@users.noreply.github.com>
2024-04-24 13:27:35 +0900

    22

--- #4.py
+++ #4.py
@@ -1,15 +1,16 @@
-animals = []
-animals.append('cat')
-animals.append('dog')
-animals.append('mouse')
+#animals = []
+#animals.append('cat')
+#animals.append('dog')
+#animals.append('mouse')
 #count=collections.Counter(animals)
 #rint(animals)
 #print(count)
 #count(animals)
 #count = [animals]
 #print(animals)len(animals)
-count = 3
-print(animals)
+#count = 3
+#print(animals)
 #print(count)
 #len(animals)
-print(count)
+#print(count)
+print("2315426")
